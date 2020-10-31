b48a200ccd70fbec1803fe65a5c48fbfbe9a5411
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2020-10-31 19:19:41 +0530

    updated comming soon page

--- Comming_soon/js/main.js
+++ Comming_soon/js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 01 2020",
+	date: "January 01 2021",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
